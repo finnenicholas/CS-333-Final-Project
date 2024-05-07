3a1d0deba5c6d31de1b2533deb7f2b0d3cf3b09a
finnenicholas <nfinne@nevada.unr.edu>
2024-05-06 23:18:09 -0700

    Adding more tests

--- tictactoe_test.py
+++ tictactoe_test.py
@@ -6,7 +6,45 @@ class TestTicTacToe(unittest.TestCase):
         self.game = TicTacToe()
 
     def test_initial_board_empty(self):
-        self.assertEqual(self.game.board, [[' ']*3, [' ']*3, [' ']*3])
+        self.assertEqual(self.game.board, [[' ']*3, [' ']*3, [' ']*3]) # That's cool how you can do the *3
+
+    def test_place_mark(self):
+        self.assertTrue(self.game.place_mark(0, 0))
+        self.assertEqual(self.game.board[0][0], 'O')
+        self.game.current_player = 1
+        self.assertTrue(self.game.place_mark(0, 1))
+        self.assertEqual(self.game.board[0][1], 'X')
+
+    def test_place_mark_on_taken_spot(self):
+        self.game.place_mark(0, 0)
+        self.assertFalse(self.game.place_mark(0, 0))
+
+    def test_validate_move_valid(self):
+        self.assertTrue(self.game.validate_move(0, 0))
+
+    def test_validate_move_invalid(self):
+        self.assertFalse(self.game.validate_move(-1, 0))
+        self.assertFalse(self.game.validate_move(3, 3))
+
+    def test_check_for_win_horizontal(self):
+        self.game.board = [['O', 'O', 'O'], [' ', ' ', ' '], [' ', ' ', ' ']]
+        self.assertTrue(self.game.check_for_win())
+
+    def test_check_for_win_vertical(self):
+        self.game.board = [['X', ' ', ' '], ['X', ' ', ' '], ['X', ' ', ' ']]
+        self.assertTrue(self.game.check_for_win())
+
+    def test_check_for_win_diagonal(self):
+        self.game.board = [['O', ' ', ' '], [' ', 'O', ' '], [' ', ' ', 'O']]
+        self.assertTrue(self.game.check_for_win())
+
+    def test_check_for_stalemate(self):
+        self.game.board = [['O', 'X', 'O'], ['O', 'O', 'X'], ['X', 'O', 'X']]
+        self.assertTrue(self.game.check_for_stalemate())
+
+    def test_no_stalemate_if_moves_left(self):
+        self.game.board = [['O', 'X', 'O'], ['O', ' ', 'X'], ['X', 'O', 'X']]
+        self.assertFalse(self.game.check_for_stalemate())
 
 if __name__ == '__main__':
     unittest.main()
