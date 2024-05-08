eac10f7649453600394e0b88981716fcb9bdb27b
finnenicholas <nfinne@nevada.unr.edu>
2024-05-07 18:20:36 -0700

    Added more tests to hit coverage.

--- tictactoe_test.py
+++ tictactoe_test.py
@@ -48,7 +48,7 @@ class TestTicTacToe(unittest.TestCase):
 
     def test_invalid_move_out_of_bounds(self):
         self.assertFalse(self.game.validate_move(-1, 0))
-        self.assertFalse(self.game.validate_move(0, 3)) 
+        self.assertFalse(self.game.validate_move(0, 3))
 
     def test_turn_switching(self):
         self.assertEqual(self.game.current_player, 0)
@@ -58,6 +58,32 @@ class TestTicTacToe(unittest.TestCase):
         self.game.place_mark(0, 1)
         self.game.next_turn()
         self.assertEqual(self.game.current_player, 0)
+
+    def test_game_reset(self):
+        self.game.board = [['X', 'O', 'X'], ['O', 'X', 'O'], ['X', 'O', 'X']]
+        self.game.game_over = True
+        self.game.__init__()
+        self.assertEqual(self.game.board, [[' ']*3, [' ']*3, [' ']*3])
+        self.assertFalse(self.game.game_over)
+
+    def test_full_game_play(self):
+        moves = [(0, 0), (1, 0), (0, 1), (1, 1), (0, 2)]  
+        for move in moves:
+            self.game.place_mark(*move)
+            self.game.next_turn()
+        self.assertTrue(self.game.check_for_win())
+
+    def test_input_conversion(self):
+        input = "1 2"
+        x, y = map(int, input.split())
+        self.assertTrue(self.game.validate_move(x, y))
+
+    def test_game_continuation_after_win(self):
+        self.game.board = [['X', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        self.game.current_player = 0
+        self.game.place_mark(0, 2)  
+        self.assertTrue(self.game.check_for_win())
+        self.assertFalse(self.game.place_mark(1, 1))  
 
 if __name__ == '__main__':
     unittest.main()
